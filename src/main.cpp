10152d065333e5d7b77a33c9d1b9fb26c47f5280
Alexander Miller <alex.miller@gmx.de>
2017-09-28 18:47:25 +0200

    Update copyright
    Add myself as author and copyright holder.

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,19 @@
-/*
+/**
+ * @mainpage
+ *
+ * @section desc Description
+ *
+ * KDing is a KDE port of
+ * <a href="http://www-user.tu-chemnitz.de/~fri/ding/">Ding</a>, a dictionary
+ * lookup program. It sits in KDE's system tray and can translate the current
+ * clipboard content. Users can also enter single words or phrases for
+ * translation. It is intended to be used for translating between German and
+ * English, but can be used with every language for which a word list is
+ * available for
+ * <a href="http://www-user.tu-chemnitz.de/~fri/ding/#other">Ding</a>.
+ *
+ * @section license License
+ *
  * Copyright (c) 2009 Michael Rex <me@rexi.org>
  * Copyright (c) 2017 Alexander Miller <alex.miller@gmx.de>
  *
@@ -34,6 +49,7 @@ int main(int argc, char* argv[]) {
     about.setLicense(KAboutLicense::GPL_V2);
     about.setCopyrightStatement(i18n(
         "(c) 2005-2009, Michael Rex\n"
+        "(c) 2017, Alexander Miller\n"
         "Dictionary (c) 1995-2016, Frank Richter"));
     about.setHomepage("https://github.com/miller-alex/kding");
     about.setBugAddress("alex.miller@gmx.de");
@@ -65,35 +81,3 @@ int main(int argc, char* argv[]) {
 
     return app.exec();
 }
-
-/**
- * @mainpage
- *
- * @section desc Description
- *
- * KDing is a KDE port of
- * <a href="http://www-user.tu-chemnitz.de/~fri/ding/">Ding</a>, a dictionary
- * lookup program. It sits in KDE's system tray and can translate the current
- * clipboard content. Users can also enter single words or phrases for
- * translation. It is intended to be used for translating between German and
- * English, but can be used with every language for which a word list is
- * available for
- * <a href="http://www-user.tu-chemnitz.de/~fri/ding/#other">Ding</a>.
- *
- * @section license License
- *
- * Copyright (c) 2009 Michael Rex <me@rexi.org>
- *
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as
- * published by the Free Software Foundation.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- */
