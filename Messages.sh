3d3760a730c3a30cc73e69bfd837159af26d5499
Alexander Miller <alex.miller@gmx.de>
2017-11-19 23:20:29 +0100

    Improve .po file generation
    Reimplement Messages.sh:
    * Get rid of temporary files.
    * Don't search dotfiles (find * instead of find .).
    * Use find -exec instead of find + xargs.
    * Postprocess output to turn extractrc's file comments
      into proper .po "#:" location comments and simplify
      double-prefix comments.
    * Let xgettext sort the entries instead of sorting the
      list of source files.
    * Check whether required utilities exist before trying
      to update .po files.
    * Accept command line arguments to let the user only
      extract messages or only update (some) .po files.
      Running without arguments still performs both steps.
    * Set project name and version in the .pot template.
      Version number gets extracted from the KAboutData in
      src/main.cpp.

--- Messages.sh
+++ Messages.sh
@@ -1,36 +1,73 @@
 #!/bin/sh
 
-PROJECT="kding"     # project name
-WORKINGDIR=`pwd`    # working directory
+cd "$(dirname "$0")"
+
+PROJECT="kding"
 PODIR="po"
+POT="${PODIR}/${PROJECT}.pot"
 
-echo "Preparing rc files"
-# we use simple sorting to make sure the lines do not jump around too much from system to system
-find . -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' | sort > ${WORKINGDIR}/rcfiles.list
-xargs --arg-file=${WORKINGDIR}/rcfiles.list extractrc > ${WORKINGDIR}/rc.cpp
-# additional strings for KAboutData
-#echo 'i18nc("NAME OF TRANSLATORS","Your names");' >> ${WORKINGDIR}/rc.cpp
-#echo 'i18nc("EMAIL OF TRANSLATORS","Your emails");' >> ${WORKINGDIR}/rc.cpp
+# extract version number from KAboutData
+VERSION="$(sed -n 's/.*about([^,]*,[^,]*, *"\([0-9.]*\)[-"].*/\1/p' \
+	< src/main.cpp)"
 
-echo "Extracting messages"
-# see above on sorting
-find . -name '*.cpp' -o -name '*.h' -o -name '*.c' | sort > ${WORKINGDIR}/infiles.list
-echo "rc.cpp" >> ${WORKINGDIR}/infiles.list
-xgettext --from-code=UTF-8 -C -kde -ci18n -ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3 -ktr2i18n:1 \
-         -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale -kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3 \
-         --files-from=infiles.list -D ${WORKINGDIR} -o ${PODIR}/${PROJECT}.pot || { echo "Error while calling xgettext. Aborting."; exit 1; }
+XGETTEXT_OPTS="-F --from-code=UTF-8 -C -kde -ci18n
+	-ki18n:1 -ki18nc:1c,2 -ki18np:1,2 -ki18ncp:1c,2,3
+	-ktr2i18n:1  -kI18N_NOOP:1 -kI18N_NOOP2:1c,2 -kaliasLocale
+	-kki18n:1 -kki18nc:1c,2 -kki18np:1,2 -kki18ncp:1c,2,3
+	--package-name=${PROJECT} --package-version=${VERSION}"
+MSGMERGE_OPTS=''
 
-echo "Merging translations"
-cd ${PODIR}
-catalogs=`find . -name '*.po'`
-for cat in $catalogs; do
-    echo "Processing ${cat}"
-    msgmerge -o $cat.new $cat ${PROJECT}.pot
-    mv $cat.new $cat
+[ $# -eq 0 ] && set -- x u
+extract= mergepo=
+while [ $# -gt 0 ]; do
+	case "$1" in
+		x|-x|extract|--extract)
+			extract=y;;
+		u|-u|update|--update|m|-m|merge|--merge)
+			mergepo="${PODIR}";;
+		update=*|--update=*|merge=*|--merge=*)
+			mergepo="${1#*=}";;
+		U|-U|M|-M)
+			shift
+			mergepo="$1";;
+		-h|--help|help)
+			echo \
+"${0##*/}: extract translatable strings from sources and update .po files
+
+Arguments:
+    x, extract    create messages template ${POT} from sources
+    u, update     merge template into all translations (.po files)
+    U, update=PO  merge translations under file/dir PO only
+Both 'extract' and 'update' commands will be performed if called without
+arguments."
+			exit;;
+		*)	echo "${0##*/}: unrecogized argument: $1" 1>&2
+			exit 1;;
+	esac
+	shift
 done
-cd ${WORKINGDIR}
 
-echo "Cleaning up"
-rm rcfiles.list
-rm infiles.list
-rm rc.cpp
+type extractrc xgettext msgmerge > /dev/null || exit 1
+
+# Note: don't run "find ." since that would search .git/, too; use "find *"
+
+if [ "${extract}" = y ]; then
+	echo ' * extract messages from resources'
+	# filter the output to obtain proper location comments
+	# (and simplify double comment prefixes while at it)
+	find * \( -name '*.rc' -o -name '*.ui' -o -name '*.kcfg' \) -type f \
+		-exec extractrc '{}' + |
+		xgettext ${XGETTEXT_OPTS} -o - - |
+		sed -e '/^#:/ d' -e 's/^#. i18n: file:/#:/' \
+		    -e 's/^#. i18n: ectx:/#. ectx:/' >| "${POT}"
+
+	echo ' * extract messages from source files'
+	find * \( -name '*.[ch]' -o -name '*.[ch]pp' \) -type f -exec \
+		xgettext -j ${XGETTEXT_OPTS} -o "${POT}" '{}' +
+fi
+
+if [ -n "${mergepo}" ]; then
+	# merge po files
+	find "${PODIR}" -name '*.po' -type f -printf ' * update %p\n' -exec \
+		msgmerge -U ${MSGMERGE_OPTS} '{}' "${POT}" \;
+fi
